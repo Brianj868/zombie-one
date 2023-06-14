812c0f6dc36489bce14d14e21d1d684e29704e0f
Brian Johnston <brianj868@yahoo.com>
2023-06-14 16:01:49 -0700

    Project Created

---
+++ ZombieOne/Intermediate/Build/Win64/ZombieOneEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30148
+#include "C:/Users/brian/OneDrive/Documents/GitHub/zombie-one/ZombieOne/Intermediate/Build/Win64/ZombieOneEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ ZombieOne/Source/ZombieOne/ZombieOne.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ZombieOne.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZombieOne, "ZombieOne" );
